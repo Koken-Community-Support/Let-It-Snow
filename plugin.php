4bb68196a7382850722c32e9791c983ee2f70d8c
Bjarne Varöystrand <black.skorpio+github@gmail.com>
2015-12-22 15:55:22 +0100

    Update 1.0.1
    Updated jquery.snow.js to the latest.
    Added two more color options: you can now have three different colors on
    the flakes.
    Added option to change the dingbat used: you now have 20 different
    characters to choose from.
    Changed the default values of the snowflake sizes a bit.
    Changed the z-index to 20 instead of 999!
    Using <div class="flake"> instead of <div=id"flake"> since ID should be
    unice.

--- plugin.php
+++ plugin.php
@@ -4,24 +4,27 @@ class BaldursPhotographyLetItSnow extends KokenPlugin {
 
 	function __construct()
 	{
-		$this->require_setup = true;
+		$this->require_setup = false;
 		$this->register_hook('before_closing_body', 'render');
 	}
 
 	function render()
 	{
 
-		$minsize	= $this->data->let_it_snow_minsize;
-		$maxsize	= $this->data->let_it_snow_maxsize;
-		$newon		= $this->data->let_it_snow_newon;
-		$flakecolor	= $this->data->let_it_snow_flakecolor;
-		$path		= $this->get_path();
+		$flakeChar		= $this->data->let_it_snow_char;
+		$minsize		= $this->data->let_it_snow_minsize;
+		$maxsize		= $this->data->let_it_snow_maxsize;
+		$newon			= $this->data->let_it_snow_newon;
+		$flakecolor1	= $this->data->let_it_snow_flakecolor1;
+		$flakecolor2	= $this->data->let_it_snow_flakecolor2;
+		$flakecolor3	= $this->data->let_it_snow_flakecolor3;
+		$path			= $this->get_path();
 
 		echo <<<OUT
-<script src="{$path}/jquery.snow.min.1.0.js"></script>
+<script src="{$path}/jquery.snow.min.js"></script>
 <script>
 	$(document).ready( function(){
-		$.fn.snow({ minSize: {$minsize}, maxSize: {$maxsize}, newOn: {$newon}, flakeColor: '{$flakecolor}' });
+		$.fn.snow({ flakeChar: "{$flakeChar}", minSize: {$minsize}, maxSize: {$maxsize}, newOn: {$newon}, flakeColor: ["{$flakecolor1}", "{$flakecolor2}", "{$flakecolor3}"] });
 	});
 </script>
 OUT;
